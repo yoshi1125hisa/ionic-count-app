ab211aaf6489c6574aab4ecbae4fca854eb7767f
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2018-11-19 17:02:28 +0900

    [Update] First commit .

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,21 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  public num: number = 0;
   constructor(public navCtrl: NavController) {
 
   }
+
+   plus() {
+       this.num++;
+         }
+
+	   minus() {
+	       this.num--;
+	         }
+
+		   reset() {
+		       this.num = 0;
+		         }
 
 }
